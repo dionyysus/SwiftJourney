4f0274deaf587a9c45e1a7a84a7c9c09a7e625de
dionyysus <gizemcsk1@gmail.com>
2023-07-08 22:57:11 +0300

    created an image array

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Gizem Coşkun on 8.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [UIImage(named: "ball1"),UIImage(named: "ball2"),UIImage(named: "ball3"),UIImage(named: "ball4"),UIImage(named: "ball5"),]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
