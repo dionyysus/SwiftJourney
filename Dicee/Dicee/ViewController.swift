1b3b24cf073019caff54ef15784ea9b55a174881
dionyysus <gizemcsk1@gmail.com>
2023-07-08 21:22:25 +0300

    dice image view changed with code

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var changeDice1 = 1
+    var changeDice2 = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+        
     }
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        let dices = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
 
+        diceImageView1.image = dices[changeDice1]
+        diceImageView2.image = dices[changeDice2]
+        
+        changeDice1 += 1
+        changeDice2 -= 1
+    }
 }
 
