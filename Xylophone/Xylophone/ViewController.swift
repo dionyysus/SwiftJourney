91878599722d71d0ad41684504ca5798dc52fe3d
dionyysus <gizemcsk1@gmail.com>
2023-07-09 15:07:55 +0300

    added music when pressed button

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Gizem Coşkun on 9.07.2023.
+//
+
+import UIKit
+import AVFoundation
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound(){
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
+
