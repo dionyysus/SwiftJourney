da108266eb3ca83717cbdb8a057e441ccc4703f7
dionyysus <gizemcsk1@gmail.com>
2023-07-11 14:35:59 +0300

    button click done

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+   
+    @IBAction func softSelected(_ sender: UIButton) {
+        print(softTime)
+    }
+    
+    @IBAction func mediumSelected(_ sender: UIButton) {
+        print(mediumTime)
+    }
+    
+    @IBAction func hardSelected(_ sender: Any) {
+        print(hardTime)
+    }
+    
 }
-
