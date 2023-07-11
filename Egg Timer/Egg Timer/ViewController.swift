9ae63dbe2cfa5386a9f6299677d5232892f377d6
dionyysus <gizemcsk1@gmail.com>
2023-07-11 14:05:20 +0300

    egg timer design completed

---
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Gizem Coşkun on 11.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
