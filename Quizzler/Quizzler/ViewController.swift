2f8a10124306162c0001a33c5480f91b5e9ceaea
dionyysus <gizemcsk1@gmail.com>
2023-07-13 10:53:09 +0300

    storyboard done

---
+++ Quizzler/Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Gizem Coşkun on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
