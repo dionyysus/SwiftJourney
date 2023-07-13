f9792e29cdf3863451199355ab141bdcf030b161
dionyysus <gizemcsk1@gmail.com>
2023-07-13 08:53:12 +0300

    timer updated and progress bar added

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -10,32 +10,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var barProgressView: UIProgressView!
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
-    
-    let eggTimes = ["Soft": 300 ,"Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 60
+    let eggTimes = ["Soft": 3 ,"Medium": 4, "Hard": 7]
+   var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func selectedButton(_ sender: UIButton) {
         
         let hardness = sender.currentTitle ?? ""
-
-        secondsRemaining = eggTimes[hardness] ?? 0
-    
+        totalTime = eggTimes[hardness] ?? 0
+      
         
         Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            let percentageProgress = secondsPassed / totalTime
+            barProgressView.progress = Float(percentageProgress)
+            secondsPassed += 1
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Done!"
         }
     }
-    
-
-   
     
 }
