87b45afe8f80945c059b1e09b646498cff0dac42
dionyysus <gizemcsk1@gmail.com>
2023-07-15 13:04:54 +0300

    added calculation button

--- BMI Calculator/BMI Calculator/Controller/ViewController.swift
+++ BMI Calculator/BMI Calculator/Controller/ViewController.swift
@@ -12,11 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         heightLabel.text = "\(String(format: "%.2f", sender.value))m"
     }
