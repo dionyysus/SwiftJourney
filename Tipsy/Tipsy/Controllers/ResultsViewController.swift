8d70372550ada66646d46f5fa34a5bf992a7004f
dionyysus <gizemcsk1@gmail.com>
2023-07-17 16:12:49 +0300

    second view controller iboutlet and ibaction created

---
+++ Tipsy/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Gizem Coşkun on 17.07.2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
