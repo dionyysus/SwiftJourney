1013c663f9ffab30f700745667ae7af987898432
dionyysus <gizemcsk1@gmail.com>
2023-07-17 11:34:58 +0300

    ibaction and iboutlet created

---
+++ Tipsy/Tipsy/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Gizem Coşkun on 17.07.2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
