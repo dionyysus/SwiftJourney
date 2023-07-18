80e9975a53fd01a6456fe663c1e91845439d8c3e
dionyysus <gizemcsk1@gmail.com>
2023-07-18 17:50:00 +0300

    iboutlet created

--- Clima/Clima/Controller/WeatherViewController.swift
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
