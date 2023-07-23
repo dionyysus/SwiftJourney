0a47d44285c4ccafb51524c3b63febf55a24e05c
dionyysus <gizemcsk1@gmail.com>
2023-07-23 16:26:12 +0300

    location press button updated

--- Clima/Clima/Controller/WeatherViewController.swift
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -29,6 +29,9 @@ class WeatherViewController: UIViewController {
         searchTextField.delegate = self
     }
 
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
 }
 
 //MARK: - UITextFieldDelegate
@@ -81,6 +84,7 @@ extension WeatherViewController: WeatherManagerDelegate {
 extension WeatherViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
+            locationManager.stopUpdatingLocation()
             let lat = location.coordinate.latitude
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(latitude: lat, longitute: lon)
