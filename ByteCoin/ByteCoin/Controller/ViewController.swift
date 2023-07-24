0897db567087af207a960bfc24a75f444f1211e5
dionyysus <gizemcsk1@gmail.com>
2023-07-24 15:30:41 +0300

    picker view delegate added

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
+    
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+}
