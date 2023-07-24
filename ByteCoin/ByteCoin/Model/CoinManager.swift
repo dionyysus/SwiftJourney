7ff9b8d3d46658ad06caf7a77a4b23b25a712c56
dionyysus <gizemcsk1@gmail.com>
2023-07-24 09:55:20 +0300

    ByteCoin project created and api keys added

---
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Gizem Coşkun on 24.07.2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
