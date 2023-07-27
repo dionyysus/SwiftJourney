6e860f7c65d6670a01b2db59465e835652fcb648
dionyysus <gizemcsk1@gmail.com>
2023-07-27 21:01:07 +0300

    completed

--- ToDoListApp/ToDoListApp/ViewController.swift
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -15,16 +15,20 @@ class ViewController: UIViewController {
         return tableView
     }()
 
+    var items = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
         title = "To Do List"
         view.addSubview(tableView)
         tableView.dataSource = self
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemsBtn))
     }
-    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
     @objc func addItemsBtn(){
         let alert = UIAlertController(title: "New Item", message: "Enter new item in todo list", preferredStyle: .alert)
         alert.addTextField { textField in
@@ -32,6 +36,18 @@ class ViewController: UIViewController {
         }
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (_) in
+            if let textField = alert.textFields?.first{
+                if let text = textField.text, !text.isEmpty {
+                    DispatchQueue.main.async {
+                        var currentItem = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItem.append(text)
+                        UserDefaults.standard.set(currentItem, forKey: "items")
+                        self.items.append(text)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+            
         }))
         present(alert, animated: true)
     }
@@ -39,11 +55,12 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
     
