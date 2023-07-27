0b223b8c204c2c5d404127bcd3bb0c2bac05fb45
dionyysus <gizemcsk1@gmail.com>
2023-07-27 20:29:50 +0300

    table view and button created programmatic ui

--- ToDoListApp/ToDoListApp/ViewController.swift
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "To Do List"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemsBtn))
     }
-
-
+    
+    @objc func addItemsBtn(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new item in todo list", preferredStyle: .alert)
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+}
