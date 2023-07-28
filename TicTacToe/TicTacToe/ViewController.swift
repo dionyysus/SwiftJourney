13c972778031b5fd23587004351fdd0a1e1ed532
dionyysus <gizemcsk1@gmail.com>
2023-07-28 20:53:37 +0300

    design completed and action&iboutlet created

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var a1Button: UIButton!
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a2Button: UIButton!
+    @IBOutlet weak var a3Button: UIButton!
+    @IBOutlet weak var b1Button: UIButton!
+    @IBOutlet weak var b3Button: UIButton!
+    @IBOutlet weak var b2Button: UIButton!
+    @IBOutlet weak var c1Button: UIButton!
+    @IBOutlet weak var c2Button: UIButton!
+    @IBOutlet weak var c3Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func boardTapAction(_ sender: UIButton) {
+    }
+    
 }
 
