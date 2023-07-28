5076b26676aba87f78bd2bbdff6048aa9b59e5e6
dionyysus <gizemcsk1@gmail.com>
2023-07-28 20:32:48 +0300

    tictactoe project created

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Gizem Coşkun on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
