663cf0a0d50a6441f893692b7394f7d7cc31b574
dionyysus <gizemcsk1@gmail.com>
2023-07-28 21:03:24 +0300

    button action updated

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Turn {
+        case Nought
+        case Cross
+    }
 
     @IBOutlet weak var a1Button: UIButton!
     @IBOutlet weak var turnLabel: UILabel!
@@ -20,12 +25,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var c2Button: UIButton!
     @IBOutlet weak var c3Button: UIButton!
     
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) {
+        addToBoard(sender)
     }
     
+    func addToBoard(_ sender: UIButton){
+        if(sender.title(for: .normal) == nil){
+            if(currentTurn == Turn.Nought){
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            } else if(currentTurn == Turn.Cross){
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+            }
+        }
+    }
 }
 
