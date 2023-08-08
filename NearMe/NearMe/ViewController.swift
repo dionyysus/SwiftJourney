65604c56c872a568bb3ea108013ce6923c25d812
dionyysus <gizemcsk1@gmail.com>
2023-08-08 09:00:39 +0300

    added map

--- NearMe/NearMe/ViewController.swift
+++ NearMe/NearMe/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.green
+        setupUI()
     }
-
-
+    
+    private func setupUI(){
+        view.addSubview(mapView)
+        
+        // add constraints to mapview
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
 }
 
