14a26a41453747ecab4d78c7e8e59e0f04022241
dionyysus <gizemcsk1@gmail.com>
2023-08-30 13:32:43 +0300

    programmatic ui started

---
+++ AutoLayoutTutorial/AutoLayoutTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutTutorial
+//
+//  Created by Gizem Coşkun on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
