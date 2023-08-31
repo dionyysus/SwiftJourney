f690318332f6189cf75c4772ea1ecf4978bf775e
dionyysus <gizemcsk1@gmail.com>
2023-08-31 19:54:58 +0300

    push navigation controller and added button programatically

---
+++ AutoLayoutTutorial/AutoLayoutTutorial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AutoLayoutTutorial
+//
+//  Created by Gizem Coşkun on 31.08.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemPurple
+    }
+    
+
+
+}
--- AutoLayoutTutorial/AutoLayoutTutorial/ViewController.swift
+++ AutoLayoutTutorial/AutoLayoutTutorial/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle("My Button", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
+        
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+            button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
+    
+    @objc func didTapButton() {
+        let vc = SecondViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
+
 
